721202d8e159537857ec4c809cfae0ec68aade79
tHeFinAlGoaT <isonobello732@gmail.com>
2022-11-06 10:59:40 +0100

    print values

--- main.py
+++ main.py
@@ -27,16 +27,16 @@ while True:
             balancedata = s.json()
             # Actually get the balance
             balance = balancedata['balance']
-            print(balance)
+            print(f'your balance is: {balance}')
             # Check if the balance is enough to buy
             if balance >= relative_price:
-                # get the actual full price of the asset 
-                price = round(relative_price, 0)
                 # Send request to buy the asset
                 x_burp0_url = "https://api.rbxflip.com:443/roblox/shop/buy"
                 x_burp0_headers = {"X-Socket-Id": "lSzxyOry5rYlgJaTAB78", "Authorization": token, "Sec-Ch-Ua-Mobile": "?0", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Safari/537.36", "Sec-Ch-Ua": "\"Chromium\";v=\"103\", \".Not/A)Brand\";v=\"99\"", "Sec-Ch-Ua-Platform": "\"Windows\"", "Content-Type": "application/json", "Accept": "*/*", "Origin": "https://www.rbxflip.com", "Sec-Fetch-Site": "same-site", "Sec-Fetch-Mode": "cors", "Sec-Fetch-Dest": "empty", "Referer": "https://www.rbxflip.com/", "Accept-Encoding": "gzip, deflate", "Accept-Language": "it-IT,it;q=0.9,en-US;q=0.8,en;q=0.7"}
-                x_burp0_json=[{"expectedPrice": price, "userAssetId": user_asset_id, "userId": user_id}]
+                x_burp0_json=[{"expectedPrice": relative_price, "userAssetId": user_asset_id, "userId": user_id}]
                 g = requests.post(x_burp0_url, headers=x_burp0_headers, json=x_burp0_json)
+                print('Bought')
             else:
-                print("daaam bro you broke")
-    time.sleep(10)
+                print(f'the item price is: {relative_price}')
+                print("bal is not enough to buy")
+    time.sleep(1)
